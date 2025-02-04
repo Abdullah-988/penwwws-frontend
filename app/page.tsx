5e4d582d6d5ccd52b80ee3f3c102635cf040da17
Abdullah Abdulkareem <channel.redrex@gmail.com>
2025-02-04 04:41:52 +0300

    Fix 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-min bg-black text-red-500 md:h-full md:w-full">
-      Penwwws! with tests
+      Penwwws! with tests and deployment
     </div>
   );
 }
