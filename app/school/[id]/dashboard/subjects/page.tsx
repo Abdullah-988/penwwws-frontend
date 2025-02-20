b2c9b2ca04b7edc84641d60abfc43ddec9fd76a6
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-02-20 21:43:06 +0300

    Implement add subject

--- app/school/[id]/dashboard/subjects/page.tsx
+++ app/school/[id]/dashboard/subjects/page.tsx
@@ -1,3 +1,17 @@
-export default function SubjectsPage() {
-  return <div>SubjectsPage</div>;
+import AddSubject from "@/components/features/dashboard/subjects/addSubject";
+
+export default async function SubjectsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const schoolId = (await params).id;
+  return (
+    <div className="relative w-full p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-xl font-semibold">Subjects</h1>
+        <AddSubject schoolId={schoolId} />
+      </div>
+    </div>
+  );
 }
