d37f43c8fbed8dd4749586b89af64afbd6afb71d
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-03-17 00:59:39 +0300

    Add invite member

---
+++ app/invite/[token]/page.tsx
@@ -0,0 +1,37 @@
+import axios from "@/lib/axiosInstance";
+import { redirect } from "next/navigation";
+import { getCookie } from "cookies-next";
+import { cookies } from "next/headers";
+import { AxiosError } from "axios";
+
+export default async function InviteTokenPage({
+  params,
+}: {
+  params: Promise<{ token: string }>;
+}) {
+  const inviteToken = (await params).token;
+  const token = await getCookie("token", { cookies });
+  let errorMessage = "";
+
+  try {
+    console.log(token);
+    await axios.post(`/invite/${inviteToken}`, {
+      headers: { Authorization: token },
+    });
+  } catch (err) {
+    const error = err as AxiosError;
+    errorMessage =
+      (error.response?.data as string) || "An unexpected error occurred.";
+    console.error(errorMessage);
+  }
+
+  if (!errorMessage) {
+    redirect("/console");
+  }
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center text-3xl">
+      <h1 className="text-destructive">{errorMessage}</h1>
+    </div>
+  );
+}
