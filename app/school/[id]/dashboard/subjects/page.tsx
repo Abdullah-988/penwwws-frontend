3dc27d3e59a171cc8c7087114195bbef43062174
Abdullah Abdulkareem <channel.redrex@gmail.com>
2025-03-25 05:57:41 +0300

    Fix 1

--- app/school/[id]/dashboard/subjects/page.tsx
+++ app/school/[id]/dashboard/subjects/page.tsx
@@ -1,6 +1,7 @@
 import SubjectsList from "@/components/features/dashboard/subjects/SubjectsList";
 import AddSubject from "@/components/features/dashboard/subjects/AddSubject";
 
+// tring to fix the error
 export default async function SubjectsPage({
   params,
 }: {
