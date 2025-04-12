b18e72e67f848d274071f1c6e097c7671f3fa7ed
Abdullah Abdulkareem <channel.redrex@gmail.com>
2025-04-13 02:07:55 +0300

    Add Landing page

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,8 +1,19 @@
-import { expect, test } from "vitest";
+import { beforeEach, expect, test, vitest } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Page from "@/app/page";
 
+beforeEach(() => {
+  const mockIntersectionObserver = vitest.fn();
+  mockIntersectionObserver.mockReturnValue({
+    observe: () => null,
+    unobserve: () => null,
+    disconnect: () => null,
+  });
+  window.IntersectionObserver = mockIntersectionObserver;
+});
+
 test("Page", () => {
   render(<Page />);
-  expect(screen.getByText("Penwwws!")).toBeDefined();
+
+  expect(screen.getByText("Penwwws")).toBeDefined();
 });
---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import PenwwwsIcon from "@/components/icons/Penwwws";
+import clsx from "clsx";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav
+      className={clsx(
+        "fixed z-20 flex w-full items-center justify-between px-8 md:px-16 lg:px-32",
+        {
+          "bg-white p-5 duration-400": scrolled,
+          "p-16 duration-200": !scrolled,
+        },
+      )}
+    >
+      <Link
+        href="/"
+        className={clsx(
+          "flex items-center justify-center gap-2 p-2 duration-200 hover:opacity-80",
+          {
+            "text-primary-900": scrolled,
+            "text-white": !scrolled,
+          },
+        )}
+      >
+        <PenwwwsIcon className="h-8 w-8" />
+        <h1 className="text-2xl font-bold">Penwwws</h1>
+      </Link>
+      <div>
+        <Link
+          className={clsx(
+            "mr-2 rounded-lg px-4 py-2 font-semibold duration-200",
+            {
+              "text-primary-900 hover:opacity-80": scrolled,
+              "text-white hover:bg-white/20": !scrolled,
+            },
+          )}
+          href="/sign-in"
+        >
+          Sign In
+        </Link>
+        <Link
+          className={clsx(
+            "rounded-lg px-4 py-2 font-semibold duration-200 hover:opacity-80",
+            {
+              "bg-primary-900 text-white": scrolled,
+              "text-primary-900 bg-white": !scrolled,
+            },
+          )}
+          href="/sign-up"
+        >
+          Sign Up
+        </Link>
+      </div>
+    </nav>
+  );
+}
