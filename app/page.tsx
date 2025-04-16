a2008aa38a5a74ffc2548ac226e15f4063a74693
Abdullah Abdulkareem <channel.redrex@gmail.com>
2025-04-17 01:45:24 +0300

    Add Q&A, get started and footer

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,13 @@ import { Marquee } from "@/components/magicui/marquee";
 import { cn } from "@/lib/utils";
 import Iphone15Pro from "@/components/magicui/iphone-15-pro";
 import { AnimatedList } from "@/components/magicui/animated-list";
+import Footer from "@/components/shared/Footer";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 type Item = {
   name: string;
@@ -30,11 +37,11 @@ type Item = {
 export default function Home() {
   const files = [
     {
-      name: "Lecture-3.pdf",
+      name: "Lecture-3.pptx",
       body: "Bitcoin is a cryptocurrency invented in 2008 by an unknown person or group of people using the name Satoshi Nakamoto.",
     },
     {
-      name: "Assignment.pptx",
+      name: "Assignment.pdf",
       body: "A spreadsheet or worksheet is a file made of rows and columns that help sort data, arrange data easily, and calculate numerical data.",
     },
     {
@@ -123,7 +130,7 @@ export default function Home() {
       <section className="from-primary-950 via-primary-500 bg-gradient-to-b from-10% via-50% to-transparent to-80% px-8 pt-48 md:px-16 lg:px-32">
         <h1
           data-testid="hero"
-          className="font-jakarta w-full text-5xl font-extrabold tracking-tight text-white/90 md:w-5/6 md:text-7xl lg:w-3/4"
+          className="font-jakarta w-full text-5xl font-extrabold tracking-tight text-white/90 md:w-6/7 md:text-6xl lg:w-5/6 lg:max-w-[1000px] lg:text-7xl"
         >
           All-in-One{" "}
           <FlipWords
@@ -134,8 +141,8 @@ export default function Home() {
           Management, Simplified.
         </h1>
         <p className="mt-6 mb-8 w-full text-sm text-white/70 md:w-4/5 md:text-lg lg:w-3/5">
-          A smart ecosystem built for schools, with seamless class scheduling,
-          and intelligent insights — all with lightning-fast performance and a
+          A smart ecosystem built for schools, with seamless subject and
+          attendance management — all with lightning-fast performance and a
           beautifully intuitive interface.
         </p>
         <Link
@@ -153,15 +160,12 @@ export default function Home() {
           alt="Dashboard"
         />
       </section>
-      <section className="mt-16 px-8 py-16 md:px-16 lg:px-32">
-        <h1 className="font-jakarta w-3/4 text-5xl leading-normal font-extrabold tracking-tight md:text-6xl">
-          {/* Designed for Educators 🧑‍🏫, Loved by Administrators 👨‍💻 */}
-        </h1>
+      <section id="features" className="mt-16 px-8 py-16 md:px-16 lg:px-32">
         <TextGenerateEffect
-          className="font-jakarta text-4xl leading-snug font-extrabold tracking-tight md:text-6xl"
+          className="font-jakarta max-w-[1200px] text-4xl leading-snug font-extrabold tracking-tight md:text-6xl"
           words="Designed for Educators 🧑‍🏫, Loved by Administrators 👨‍💻"
         />
-        <p className="mt-4 w-2/5 text-xl opacity-60">
+        <p className="mt-4 text-xl opacity-60 lg:w-3/5">
           Penwwws is packed with everything you need to handle the day-to-day,
           all in one intuitive platform.
         </p>
@@ -171,8 +175,6 @@ export default function Home() {
               Icon={File}
               name="File Sharing"
               description="Share lecture notes, assignments, and other files with students."
-              href="/"
-              cta="Learn more"
               className="col-span-3 lg:col-span-2"
               background={
                 <Marquee
@@ -205,23 +207,20 @@ export default function Home() {
               Icon={TabletSmartphone}
               name="Responsive UI"
               description="Responsive design for all devices, from mobile to desktop."
-              href="/"
               background={
                 <div className="absolute flex h-full w-full items-center justify-center duration-200 [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] hover:scale-110">
                   <Iphone15Pro
                     className="h-72 w-64"
-                    src="/images/dashboard.png"
+                    src="/images/dashboard-mobile.png"
                   />
                 </div>
               }
-              cta="Learn more"
               className="col-span-3 lg:col-span-1"
             />
             <BentoCard
               Icon={NotebookPen}
               name="Grade Management"
               description="Marks and grades management for students, with easy access to all data."
-              href="/"
               background={
                 <div className="absolute flex h-full w-full items-center duration-200 [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] hover:scale-110">
                   <div className="absolute mx-8 flex w-full -translate-y-10 flex-col rounded-lg bg-neutral-100 py-1 lg:right-20 lg:m-0 lg:translate-x-1/2">
@@ -244,14 +243,12 @@ export default function Home() {
                   </div>
                 </div>
               }
-              cta="Learn more"
               className="col-span-3 lg:col-span-1"
             />
             <BentoCard
               Icon={Fingerprint}
               name="Attendance Tracking"
               description="Track attendance with ease using biometric recognition."
-              href="/"
               background={
                 <>
                   <AnimatedList
@@ -264,12 +261,81 @@ export default function Home() {
                   </AnimatedList>
                 </>
               }
-              cta="Learn more"
               className="col-span-3 lg:col-span-2"
             />
           </BentoGrid>
         </div>
       </section>
+      <section
+        id="faq"
+        className="flex flex-col justify-between gap-8 p-8 py-16 pt-32 md:flex-row md:px-16 lg:px-32"
+      >
+        <h1 className="font-jakarta text-5xl font-bold md:text-7xl">Q&A</h1>
+        <div>
+          <Accordion
+            className="border-b md:w-[500px]"
+            type="single"
+            collapsible
+          >
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="text-xl font-semibold">
+                What is Penwwws?
+              </AccordionTrigger>
+              <AccordionContent className="pb-4 pl-7">
+                Penwwws is an all-in-one platform to manage students, staff,
+                classes, and school operations.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+          <Accordion
+            className="border-b md:w-[500px]"
+            type="single"
+            collapsible
+          >
+            <AccordionItem value="item-2">
+              <AccordionTrigger className="text-xl font-semibold">
+                Is it free?
+              </AccordionTrigger>
+              <AccordionContent className="pb-4 pl-7">
+                Penwwws has a free plan to explore and get started.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+          <Accordion
+            className="border-b md:w-[500px]"
+            type="single"
+            collapsible
+          >
+            <AccordionItem value="item-2">
+              <AccordionTrigger className="text-xl font-semibold">
+                Is it easy to use?
+              </AccordionTrigger>
+              <AccordionContent className="pb-4 pl-7">
+                Penwwws is designed to be user-friendly and intuitive, making it
+                easy for anyone to use.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </section>
+      <section className="p-8 text-white md:px-16">
+        <div className="bg-primary-900 rounded-xl p-8 md:p-16">
+          <h2 className="font-jakarta text-xl font-bold sm:text-4xl sm:leading-12 md:text-6xl md:leading-16 lg:w-1/2">
+            Ready to Run Your School Smarter?
+          </h2>
+          <p className="mb-4 text-sm sm:mt-6 sm:text-lg md:mb-8 lg:w-1/2">
+            From enrollment to attendance, get everything set up quickly and
+            start managing with confidence.
+          </p>
+          <Link
+            href="/sign-in"
+            className="text-primary-900 rounded-md bg-white p-2 px-3 text-sm font-semibold duration-200 hover:opacity-90 md:p-3 md:px-5 md:text-base"
+          >
+            Get Started
+          </Link>
+        </div>
+      </section>
+      <Footer />
     </>
   );
 }
