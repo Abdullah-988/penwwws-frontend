733be3f19db7b520bc86c32a02d65d70d5ba7af6
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-05-04 10:07:20 +0300

    Fix Google OAuth redirection

--- components/features/auth/GoogleOAuthBtn.tsx
+++ components/features/auth/GoogleOAuthBtn.tsx
@@ -6,7 +6,9 @@ type Props = {
 };
 
 export default function GoogleOAuthBtn({ redirectUrl }: Props) {
-  const URL = `https://accounts.google.com/o/oauth2/auth?redirect_uri=https://penwwws.com/auth/session/callback&response_type=token&client_id=${process.env.GOOGLE_CLIENT_ID}&scope=openid%20email%20profile&state=${redirectUrl}`;
+  const state = !!redirectUrl ? `&state=${redirectUrl}` : "";
+
+  const URL = `https://accounts.google.com/o/oauth2/auth?redirect_uri=http://localhost:3000/auth/session/callback&response_type=token&client_id=${process.env.GOOGLE_CLIENT_ID}&scope=openid%20email%20profile${state}`;
   return (
     <button className="hover:bg-muted w-full cursor-pointer rounded-md border font-semibold duration-100">
       <Link href={URL} className="flex items-center justify-center p-1">
